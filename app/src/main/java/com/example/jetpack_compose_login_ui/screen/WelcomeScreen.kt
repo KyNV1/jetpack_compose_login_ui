ce3d584e05ad481234dafaa5c2aff9ce26b181a5
KyNV1 <anhkyitfsoft@gmail.com>
2024-11-06 15:27:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jetpack_compose_login_ui/screen/WelcomeScreen.kt
@@ -0,0 +1,16 @@
+package com.example.jetpack_compose_login_ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WelcomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+
+    ) {
+        
+    }
+}
